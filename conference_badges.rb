9bea724c985ab626eb2154e2f1937018002709ca
Ethan <ethansagin@gmail.com>
2019-03-12 20:46:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,6 +12,6 @@ end
 
 def assign_rooms(array)
   new_array = []
-  array.each do |name|
+  array.each_with_index do |name, index|
   new_array << "Hello, #{name}! You'll be assigned to room #{}!"
 end
